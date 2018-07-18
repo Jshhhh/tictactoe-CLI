80f3cd947d1b47681a9926b6773ccd338e0389b8
Justin Shih <justinshih@Justins-MacBook-Pro.local>
2018-07-18 14:41:04 -0700

    Adds win verification functions

--- index.js
+++ index.js
@@ -4,6 +4,47 @@ let player1 = '';
 let player2 = '';
 let turn = true;
 
+const checkRows = () => {
+  for (let i = 0; i < 3; i++) {
+    let current = board[i][0];
+    let win = true;
+    for (let j = 1; j < 3; j++) {
+      if (board[i][j] !== current) {
+        win = false;
+      }
+    }
+    return win || false;
+  }
+};
+
+const checkColumns = () => {
+  for (let i = 0; i < 3; i++) {
+    let current = board[0][i];
+    let win = true;
+    for (let j = 1; j < 3; j++) {
+      if (board[j][i] !== current) {
+        win = false;
+      }
+    }
+    return win || false;
+  }
+};
+
+const checkDiag = () => {
+  let middle = board[1][1];
+  if (board[0][0] === middle && middle === board[2][2]) {
+    return true;
+  } else if (board[0][2] === middle && middle === board[2][0]) {
+    return true
+  }
+  return false;
+};
+
+const checkWin = () => {
+  console.log(checkColumns(), checkRows(), checkDiag());
+  return checkColumns() || checkRows() || checkDiag();
+};
+
 const displayBoard = () => {
   let string = '';
   for (let i = 0; i < 3; i++) {
@@ -24,7 +65,7 @@ const displayBoard = () => {
 prompt.start();
 
 //starts move for current player
-const playerMove = (turn) => {
+const playerMove = () => {
   prompt.get({
     name: turn ? `${player1} move` : `${player2} move`,
     type: 'string',
@@ -34,9 +75,15 @@ const playerMove = (turn) => {
     let move = result[`${currentPlayer} move`].split('').map(elem => parseInt(elem) - 1);
     if (!board[move[0]][move[1]] && board[move[0]][move[1]] !== undefined) {
       board[move[0]][move[1]] = turn ? 'X' : 'O';
-      turn = !turn;
       displayBoard();
-      playerMove(turn);
+      //if win then stop game
+      if (checkWin()) {
+        console.log(currentPlayer, 'wins');
+      } else {
+        turn = !turn;
+        displayBoard();
+        playerMove(turn);
+      }
     } else {
       console.log('Invalid Move');
       playerMove();
